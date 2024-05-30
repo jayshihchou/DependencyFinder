6fbfe41f09937c5cb7ca5fcadd717606e99fd3fb
Jay Chou <jay.chou@xrspace.io>
2024-05-30 14:46:16 +0800

    add reference mode

--- Editor/DependencyFinder.cs
+++ Editor/DependencyFinder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 public class DependencyFinder : EditorWindow
 {
@@ -34,8 +35,62 @@ public class DependencyFinder : EditorWindow
         return show;
     }
 
+    
+    bool ToggleButton(bool was, string name, GUIStyle style, params GUILayoutOption[] options)
+    {
+        var later = GUILayout.Toggle(was, name, style, options);
+        if (later != was)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool ToggleButton(bool was, string name, params GUILayoutOption[] options)
+    {
+        var later = GUILayout.Toggle(was, name, options);
+        if (later != was)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool referenceMode = false;
+    bool drawNamespace = false;
+
+    void DrawToolBar()
+    {
+        GUILayout.BeginHorizontal(EditorStyles.toolbar);
+        if (ToggleButton(referenceMode, "Reference Mode", EditorStyles.toolbarButton))
+        {
+            referenceMode = !referenceMode;
+        }
+        GUILayout.FlexibleSpace();
+        if (ToggleButton(drawNamespace, "Draw Namespace", EditorStyles.toolbarButton))
+        {
+            drawNamespace = !drawNamespace;
+        }
+        GUILayout.EndHorizontal();
+    }
+
     void OnGUI()
     {
+        DrawToolBar();
+        if (referenceMode)
+        {
+            DrawReferenceFinder();
+        }
+        else
+        {
+            DrawDependencyFinder();
+        }
+    }
+
+    #region === Dependency Finder ===
+    private void DrawDependencyFinder()
+    {
+        
         if (lastSelected != Selection.activeObject)
         {
             lastSelected = Selection.activeObject;
@@ -124,7 +179,8 @@ public class DependencyFinder : EditorWindow
                 GUILayout.BeginHorizontal();
             }
             GUI.color = type == filter ? Color.green : originalColor;
-            if (GUILayout.Button(type.Name, GUILayout.Height(buttonHeight)))
+            var typeName = drawNamespace ? type.FullName : type.Name;
+            if (GUILayout.Button(typeName, GUILayout.Height(buttonHeight)))
             {
                 filter = type;
             }
@@ -368,4 +424,544 @@ public class DependencyFinder : EditorWindow
             }
         }
     }
+    #endregion
+    #region === Reference Finder ===
+
+    [System.Serializable]
+    public class UsageMapData
+    {
+        public string FieldName;
+        public string FieldType;
+        public object referencedBy;
+        public string referencedName;
+    }
+
+    public enum ShaderPropertyType
+    {
+        Int = 0,
+        Float,
+        Color,
+        Vector,
+        Texture,
+        Count
+    }
+    [System.Serializable]
+    public class ShaderProperties
+    {
+        public string ShaderName;
+        public string[] Properties;
+        public int[] PropertyTypes;
+
+        public bool HasProperty(string name, ShaderPropertyType type)
+        {
+            for (int i = Properties.Length - 1; i >= 0; --i)
+            {
+                string property = Properties[i];
+                if (property == name)
+                {
+                    return PropertyTypes[i] == (int)type;
+                }
+            }
+            return false;
+        }
+
+        public bool HasProperty(int id, ShaderPropertyType type)
+        {
+            for (int i = Properties.Length - 1; i >= 0; --i)
+            {
+                int propertyID = Shader.PropertyToID(Properties[i]);
+                if (propertyID == id)
+                {
+                    return PropertyTypes[i] == (int)type;
+                }
+            }
+            return false;
+        }
+    }
+
+    Rect lastScrollViewRect;
+    Vector2 dependenciesScroll;
+    Dictionary<Object, List<UsageMapData>> usageMap = new();
+
+    List<UsageMapData> inspectList = null;
+    string searchBarStr = string.Empty;
+
+    bool drawTypeField;
+    string typeSearchBarStr = string.Empty;
+    HashSet<System.Type> listTypes = new HashSet<System.Type>();
+    System.Type selectedType = null;
+    string selectedTypeFullName = string.Empty;
+
+    private void DrawReferenceFinder()
+    {
+        if (ToggleButton(drawTypeField, drawTypeField ? "\u25BC Type Field" : "\u25BA Type Field", GUI.skin.label))
+        {
+            drawTypeField = !drawTypeField;
+        }
+        if (drawTypeField)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label($"Loaded Type Count: ({allTypes.Count})");
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Search Type:", GUILayout.Width(100f));
+            var newStr = GUILayout.TextField(typeSearchBarStr);
+            if (newStr != typeSearchBarStr)
+            {
+                typeSearchBarStr = newStr;
+                if (!string.IsNullOrEmpty(typeSearchBarStr))
+                {
+                    MatchSearchBar(typeSearchBarStr);
+                }
+            }
+            GUILayout.EndHorizontal();
+
+            typeScroll = EditorGUILayout.BeginScrollView(typeScroll, GUILayout.MinHeight(50), GUILayout.MaxHeight(200));
+            GUILayout.Label("Types:");
+            var originalColor = GUI.color;
+
+            int colMax = (int)(this.position.width / buttonWidth);
+            int colCount = -1;
+            GUILayout.BeginHorizontal();
+            foreach (var type in listTypes)
+            {
+                if (++colCount == colMax)
+                {
+                    colCount = 0;
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                }
+                GUI.color = type == selectedType ? Color.green : originalColor;
+                var typeName = drawNamespace ? type.FullName : type.Name;
+                if (GUILayout.Button(typeName, GUILayout.Height(buttonHeight)))
+                {
+                    selectedType = type;
+                    selectedTypeFullName = selectedType.FullName;
+                }
+            }
+            GUI.color = originalColor;
+            GUILayout.EndHorizontal();
+            EditorGUILayout.EndScrollView();
+        }
+
+        if (selectedType == null)
+        {
+            if (!string.IsNullOrEmpty(selectedTypeFullName))
+            {
+                selectedType = System.Type.GetType(selectedTypeFullName);
+            }
+            EditorGUILayout.HelpBox("No Type Selected. Select type from Type Field first.", MessageType.Warning);
+            return;
+        }
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Collect Usage"))
+        {
+            CollectReferenceFromType(selectedType);
+        }
+        if (GUILayout.Button("Clear", GUILayout.Width(80f)))
+        {
+            usageMap.Clear();
+            inspectList = null;
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label($"Search in ({usageMap.Count}) Objects:");
+        searchBarStr = GUILayout.TextField(searchBarStr);
+        GUILayout.EndHorizontal();
+        var searchBarStrLower = searchBarStr.ToLowerInvariant();
+
+        scroll = GUILayout.BeginScrollView(scroll);
+        foreach (var pair in usageMap)
+        {
+            var obj = pair.Key;
+            var key = obj == null ? "null" : obj.name;
+            if (!string.IsNullOrEmpty(searchBarStrLower) &&
+                !key.Contains(searchBarStrLower) &&
+                !searchBarStrLower.Contains(key))
+                continue;
+            var rect = GUILayoutUtility.GetRect(position.width - 5, 20f);
+            var p = new Vector2(rect.x + 1f, rect.y + 1f + lastScrollViewRect.y - scroll.y);
+            var p2 = new Vector2(rect.width - 1f, rect.height - 1f) + p;
+            var inView = lastScrollViewRect.Contains(p) || lastScrollViewRect.Contains(p2);
+            if (inView)
+            {
+                var pos = rect.position;
+                var size = rect.size;
+                size.x = size.x * 0.5f;
+                var left = new Rect(pos, size);
+                pos.x += size.x;
+                var right = new Rect(pos, size);
+
+                var typeName = obj == null ? "null" : obj.GetType().Name;
+
+                if (GUI.Button(left, $"{key} ({typeName})"))
+                {
+                    inspectList = pair.Value;
+                }
+                EditorGUI.ObjectField(right, obj, typeof(Object), true);
+            }
+        }
+        GUILayout.EndScrollView();
+        if (Event.current.type == EventType.Repaint)
+            lastScrollViewRect = GUILayoutUtility.GetLastRect();
+        if (inspectList != null)
+        {
+            dependenciesScroll = GUILayout.BeginScrollView(dependenciesScroll);
+            foreach (var obj in inspectList)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(20f);
+                GUILayout.Label($"Field Name: {obj.FieldName} ({obj.FieldType}) Referenced by:");
+                if (obj.referencedBy is Object @object)
+                {
+                    EditorGUILayout.ObjectField(@object, typeof(Object), true);
+                }
+                else
+                {
+                    GUILayout.Label(obj.referencedName);
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+    }
+
+    public IEnumerable<System.Type> FindDerivedTypes(Assembly assembly, System.Type baseType)
+    {
+        return assembly.GetTypes().Where(t => baseType.IsAssignableFrom(t));
+    }
+
+    HashSet<System.Type> allTypes = new HashSet<System.Type>();
+    void SearchTypes()
+    {
+        allTypes.Clear();
+        var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+        foreach (var assembly in assemblies)
+        {
+            foreach (var type in FindDerivedTypes(assembly, typeof(UnityEngine.Object)))
+            {
+                allTypes.Add(type);
+            }
+        }
+    }
+
+    void MatchSearchBar(string str)
+    {
+        str = str.ToLowerInvariant();
+        if (string.IsNullOrEmpty(str))
+        {
+            return;
+        }
+
+        if (allTypes.Count == 0)
+        {
+            SearchTypes();
+        }
+
+        listTypes.Clear();
+        foreach (var type in allTypes)
+        {
+            if (type == null) continue;
+            var lower = type.FullName.ToLowerInvariant();
+            if (string.IsNullOrEmpty(lower) || (!str.Contains(lower) && !lower.Contains(str)))
+            {
+                continue;
+            }
+
+            listTypes.Add(type);
+        }
+    }
+
+    void CollectReferenceFromType(System.Type type)
+    {
+        usageMap.Clear();
+        var managers = GameObject.FindObjectsOfType(type);
+        foreach (var manager in managers)
+        {
+            try
+            {
+                CollectReference(manager.name, manager);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+        }
+    }
+
+    void CollectUsageMap(Object obj, FieldInfo field, object referencedBy, string referencedName)
+    {
+        if (!usageMap.ContainsKey(obj)) usageMap[obj] = new();
+        usageMap[obj].Add(new UsageMapData()
+        {
+            FieldName = field.Name,
+            FieldType = field.FieldType.Name,
+            referencedBy = referencedBy,
+            referencedName = referencedName,
+        });
+    }
+
+    void CollectUsageMap(Object obj, PropertyInfo field, object referencedBy, string referencedName)
+    {
+        if (!usageMap.ContainsKey(obj)) usageMap[obj] = new();
+        usageMap[obj].Add(new UsageMapData()
+        {
+            FieldName = field.Name,
+            FieldType = field.PropertyType.Name,
+            referencedBy = referencedBy,
+            referencedName = referencedName,
+        });
+    }
+
+    void CollectUsageMapTexture(Object obj, string propertyName, object referencedBy, string referencedName)
+    {
+        if (!usageMap.ContainsKey(obj)) usageMap[obj] = new();
+        usageMap[obj].Add(new UsageMapData()
+        {
+            FieldName = $"{propertyName}",
+            FieldType = "Material.Texture",
+            referencedBy = referencedBy,
+            referencedName = referencedName,
+        });
+    }
+
+    static ShaderPropertyType UnityShaderPropertyTypeConvert(ShaderUtil.ShaderPropertyType type)
+    {
+        switch (type)
+        {
+            case ShaderUtil.ShaderPropertyType.Color:
+                return ShaderPropertyType.Color;
+            case ShaderUtil.ShaderPropertyType.Vector:
+                return ShaderPropertyType.Vector;
+#if UNITY_2021 || UNITY_2021_1_OR_NEWER
+            case ShaderUtil.ShaderPropertyType.Int:
+                return ShaderPropertyType.Int;
+#endif
+            case ShaderUtil.ShaderPropertyType.Float:
+            case ShaderUtil.ShaderPropertyType.Range:
+                return ShaderPropertyType.Float;
+            case ShaderUtil.ShaderPropertyType.TexEnv:
+                return ShaderPropertyType.Texture;
+        }
+        throw new System.Exception($"Unknown Shader type: {type}");
+    }
+
+    public static ShaderProperties CollectShaderProperties(Shader shader)
+    {
+        var count = ShaderUtil.GetPropertyCount(shader);
+        var properties = new ShaderProperties
+        {
+            ShaderName = shader.name,
+            Properties = new string[count],
+            PropertyTypes = new int[count],
+        };
+        for (int i = 0; i < count; ++i)
+        {
+            properties.Properties[i] = ShaderUtil.GetPropertyName(shader, i);
+            properties.PropertyTypes[i] = (int)UnityShaderPropertyTypeConvert(ShaderUtil.GetPropertyType(shader, i));
+        }
+        return properties;
+    }
+
+    void CollectForMaterial(string inputName, Material mat)
+    {
+        var property = CollectShaderProperties(mat.shader);
+        for (int i = property.Properties.Length - 1; i >= 0; --i)
+        {
+            var type = property.PropertyTypes[i];
+            if (type == (int)ShaderPropertyType.Texture)
+            {
+                var propName = property.Properties[i];
+                var tex = mat.GetTexture(propName);
+                if (tex != null)
+                {
+                    Debug.Log($"<color=cyan>Collect Texture: {tex}</color>");
+                    CollectUsageMapTexture(tex, propName, mat, inputName);
+                }
+            }
+        }
+    }
+
+    bool IsSkipType(System.Type type)
+    {
+        if (type == null) return true;
+        if (!string.IsNullOrEmpty(type.Namespace) && type.Namespace.StartsWith("System"))
+        {
+            if (type.IsArray && !IsSkipType(type.GetElementType()))
+            {
+                return false;
+            }
+
+            var args = type.GetGenericArguments();
+            foreach (var arg in args)
+            {
+                if (!IsSkipType(arg))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    void CollectReference(string inputName, object input, HashSet<object> collected = null)
+    {
+        collected ??= new HashSet<object>();
+        if (input == null)
+        {
+            return;
+        }
+
+        var type = input.GetType();
+
+        if (IsSkipType(type))
+        {
+            // Debug.Log($"Skipped: {type}, {type.Name}, {type.FullName}");
+            return;
+        }
+
+        if (type.GetCustomAttribute(typeof(System.ObsoleteAttribute)) != null)
+        {
+            return;
+        }
+
+        Debug.LogWarning($"CollectReference: {input} (Name: {inputName}, Type: {type})");
+        if (!collected.Add(input))
+        {
+            return;
+        }
+
+        if (input is Material mat)
+        {
+            try
+            {
+                CollectForMaterial(inputName, mat);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+            return;
+        }
+
+        if (input is IEnumerable collection)
+        {
+            try
+            {
+                int count = 0;
+                foreach (var item in collection)
+                {
+                    if (item == null)
+                    {
+                        continue;
+                    }
+
+                    CollectReference($"{inputName}_{count++}", item, collected);
+                }
+            }
+            catch (UnityEngine.MissingReferenceException)
+            {
+                // Nothing to do
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+            return;
+        }
+
+        var members = type.GetMembers(
+            BindingFlags.Instance |
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.GetField |
+            BindingFlags.GetProperty
+        );
+        foreach (var member in members)
+        {
+            if (member.GetCustomAttribute(typeof(System.ObsoleteAttribute)) != null)
+            {
+                continue;
+            }
+
+            if (member.MemberType == MemberTypes.Field && member is FieldInfo field)
+            {
+                try
+                {
+                    CollectField(field, inputName, input, collected);
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogError(ex);
+                }
+            }
+            else if (member.MemberType != MemberTypes.Property && member is PropertyInfo property)
+            {
+                if (property.GetIndexParameters().Any())
+                {
+                    continue;
+                }
+
+                try
+                {
+                    CollectProperty(property, inputName, input, collected);
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogError(ex);
+                }
+            }
+        }
+    }
+
+    void CollectField(FieldInfo field, string inputName, object input, HashSet<object> collected)
+    {
+        var val = field.GetValue(input);
+        if (val == null)
+        {
+            return;
+        }
+
+        Debug.Log($"field: {field.Name} val: {val}");
+        if (val is Object obj)
+        {
+            if (obj != null)
+            {
+                Debug.Log($"<color=cyan>Collect: {obj}</color>");
+                CollectUsageMap(obj, field, input, inputName);
+            }
+        }
+        if (val != input)
+        {
+            CollectReference(field.Name, val, collected);
+        }
+    }
+
+    void CollectProperty(PropertyInfo property, string inputName, object input, HashSet<object> collected)
+    {
+        var val = property.GetValue(input);
+        if (val == null)
+        {
+            return;
+        }
+
+        Debug.Log($"property: {property.Name} val: {val}");
+        if (val is Object obj)
+        {
+            if (obj != null)
+            {
+                Debug.Log($"<color=cyan>Collect: {obj}</color>");
+                CollectUsageMap(obj, property, input, inputName);
+            }
+        }
+        if (val != input)
+        {
+            CollectReference(property.Name, val, collected);
+        }
+    }
+    #endregion
 }
